a177e6e683934de2be225d64a5f03ecf4a07211a
Adithya Ramanathan <adithram@umich.edu>
2017-04-03 16:44:14 -0400

    Commenting grouping

--- static/Group.js
+++ static/Group.js
@@ -24,6 +24,7 @@
 function Group(sub_items) {
     assert_new.check(this);
     
+    // Default values
     var m_sub_items      = sub_items;
     var m_top_left       = undefined;
     var m_bottom_right   = undefined;
@@ -52,6 +53,7 @@ function Group(sub_items) {
      *                         Control point events
      **************************************************************************/
     
+    // Move points such thta they are relevant after grouping. 
     var on_move_control_points = function(displacement) {
         // need to know how to move everything in m_sub_items
         var move_points = function(points) {
@@ -74,6 +76,7 @@ function Group(sub_items) {
             });
             return items;
         };
+        // expose function used too create grouped object from individual items. 
         m_sub_items.forEach(function(item) {
             item.expose(function(data) {
                 data.points = move_points(data.points);
@@ -84,6 +87,7 @@ function Group(sub_items) {
         });
     };
     
+    // Scaling based on movement on grouped object. 
     var on_scale_control_points = function(scale_factor) {
         // Scaling is easy, but there needs to be an anchor point
         // (we also must adjust...)
@@ -95,15 +99,18 @@ function Group(sub_items) {
      *                          Public interface
      **************************************************************************/
     
+    // When line is selected, show control points. 
     this.highlight = function() {
         m_control_points = 
             new RectangularControlPoints(m_top_left, m_bottom_right);
     }
     
+    // Unhighlight when done. 
     this.unhighlight = function() {
         m_control_points = undefined;
     }
     
+    // Point detection to dictate behavior. 
     this.point_within = function(point, distance_limit) {
         if (Vector.in_bounds(point, this.bounds())) return true;
         var dist = Math.min( Math.abs(m_top_left.x     - point.x),
@@ -115,6 +122,7 @@ function Group(sub_items) {
     
     this.explode = function() { return m_sub_items; }
     
+    // Draws the items. 
     this.draw = function(context) { 
         //context.save();
         //context.scale(m_scale.x, m_scale.y);
@@ -124,22 +132,26 @@ function Group(sub_items) {
             m_control_points.draw(context);
     }
     
+    // Bounding.
     this.bounds = function() {
         return { x: m_top_left.x, y: m_top_left.y,
                  width : m_bottom_right.x - m_top_left.x,
                  height: m_bottom_right.y - m_top_left.y };
     }
     
+    // Cursor that handles cursor click - i.e. object selection for grouping. 
     this.handle_cursor_click = function(cursor_obj) {
         if (!m_is_editing) return;
         m_control_points.handle_cursor_click(cursor_obj);
     }
     
+    //Handles grouping related cursor movement. 
     this.handle_cursor_move  = function(cursor_obj) {
         if (!m_is_editing) return;
         m_control_points.handle_cursor_move(cursor_obj);
     }
     
+    // Enables editing on grouped object
     this.enable_editing = function() {
         this.highlight();
         m_is_editing = true;
@@ -147,13 +159,14 @@ function Group(sub_items) {
         m_control_points.set_move_event(on_move_control_points);
     }
     
+    // Disables editing on grouped object. 
     this.disable_editing = function () {
         this.unhighlight();
         m_is_editing = false;
     }
     
     //this.finished_creating = function() { return true; }
-    
+    // Expose serializes object for use with other objects. 
     this.expose = function(func) {
         var gv = { type : "Group", items : [] };
         m_sub_items.forEach(function(item) {
